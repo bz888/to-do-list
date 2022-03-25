ce2f21b7610ec603fee95a225547c008682c7502
bz888 <ziyu.ben158@gmail.com>
2022-03-25 19:40:53 +1300

    nuke

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import { HashRouter as Router } from 'react-router-dom'
+
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
+  render(
     <Router>
       <App />
     </Router>,
-    document.getElementById('root')
+    document.getElementById('app')
   )
 })
