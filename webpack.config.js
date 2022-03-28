9bd89e09026ae9ce1274208bc61f43e4e178b9bc
bz888 <ziyu.ben158@gmail.com>
2022-03-29 11:12:12 +1300

    setup working

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = {
+  entry: './client/index.tsx',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'server/public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        options: {
+          // disable type checker - we will use it in fork plugin
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devtool: 'source-map',
+}
