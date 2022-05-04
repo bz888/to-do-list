8f8325dc367e1a92c980294b3be262214d84573a
bz888 <ziyu.ben158@gmail.com>
2022-05-04 22:29:41 +1200

    webpack prod modification

--- webpack.config.js
+++ webpack.config.js
@@ -4,30 +4,40 @@ const path = require('path')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 // const Dotenv = require('dotenv-webpack')
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-module.exports = {
-  entry: './client/index.tsx',
-  mode: 'development',
-  output: {
-    path: path.join(__dirname, './server/public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'ts-loader',
-        test: /\.(ts|js)x?$/,
-        exclude: path.join(__dirname, './node_modules'),
-        options: {
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' })
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' })
+}
+
+module.exports = (env) => {
+  const isProduction = env === 'production'
+  return {
+    entry: './client/index.tsx',
+    mode: isProduction ? 'production' : 'development',
+    output: {
+      path: path.join(__dirname, './server/public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'ts-loader',
+          test: /\.(ts|js)x?$/,
+          exclude: path.join(__dirname, './node_modules'),
+          options: {
           // disable type checker - we will use it in fork plugin
-          transpileOnly: true
+            transpileOnly: true
+          }
         }
-      }
-    ]
-  },
-  plugins: [new ForkTsCheckerWebpackPlugin()],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
-  devtool: 'source-map'
+      ]
+    },
+    plugins: [new ForkTsCheckerWebpackPlugin()],
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
+    devtool: 'source-map'
+  }
 }
