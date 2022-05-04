e6b87cde0b37a9f05dc5dcd96d666e23bc83ef8a
bz888 <ziyu.ben158@gmail.com>
2022-05-05 01:47:30 +1200

    init heroku

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,11 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 // const Dotenv = require('dotenv-webpack')
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test' })
-} else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.development' })
-}
+// if (process.env.NODE_ENV === 'test') {
+//   require('dotenv').config({ path: '.env.test' })
+// } else if (process.env.NODE_ENV === 'development') {
+//   require('dotenv').config({ path: '.env.development' })
+// }
 
 module.exports = (env) => {
   const isProduction = env === 'production'
