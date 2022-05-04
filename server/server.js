12aa0f0fe48740852ab7c13b08c36de08a0175a8
bz888 <ziyu.ben158@gmail.com>
2022-05-04 17:15:33 +1200

    qg setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+/* eslint-disable node/no-deprecated-api */
 const path = require('path')
 const express = require('express')
 const { default: mongoose } = require('mongoose')
 require('dotenv').config()
+require('dotenv').load({ silent: true })
 
 // App Config
 const server = express()
@@ -22,6 +24,33 @@ mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.error(err))
+
+// IP address deployment
+const url = require('url')
+const HttpsProxyAgent = require('https-proxy-agent')
+const request = require('request')
+
+const testEndPoint = 'https://ip.quotaguard.com'
+const proxy = process.env.env.QUOTAGUARDSHIELD_URL
+const agent = new HttpsProxyAgent(proxy)
+const options = {
+  uri: url.parse(testEndPoint),
+  agent
+}
+
+// async function callback (error, response, body) {
+//   try {
+//     if (!error && response.statusCode === 200) {
+//       console.log('body ', body)
+//     } else {
+//       throw error
+//     }
+//   } catch {
+//     console.log('error: ', error)
+//   }
+// }
+
+// request(options, callback)
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
