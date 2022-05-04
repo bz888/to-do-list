b718eef41ec9517f45ac7a0f69be126ebf80963c
bz888 <ziyu.ben158@gmail.com>
2022-05-04 20:02:32 +1200

    webpack path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './client/index.tsx',
   mode: 'development',
   output: {
-    path: path.join(__dirname, 'server/public'),
+    path: path.join(__dirname, './server/public'),
     filename: 'bundle.js'
   },
   module: {
