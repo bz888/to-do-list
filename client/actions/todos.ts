a0481d80b2aa50e5132e7dd96dfca00c464fb649
bz888 <ziyu.ben158@gmail.com>
2022-05-05 12:18:10 +1200

    changes to dependencies

--- client/actions/todos.ts
+++ client/actions/todos.ts
@@ -1,51 +1,51 @@
-import { getAllTodosAPI } from '../api/todos'
-import { DispatchTodoType, todoItem } from '../types/types'
-import { showError } from './error'
+// import { getAllTodosAPI } from '../api/todos'
+// import { DispatchTodoType, todoItem } from '../types/types'
+// import { showError } from './error'
 
-export const TODOLIST_PENDING = 'TODOLIST_PENDING'
-export const TODOLIST_SUCCESS = 'TODOLIST_SUCCESS'
+// export const TODOLIST_PENDING = 'TODOLIST_PENDING'
+// export const TODOLIST_SUCCESS = 'TODOLIST_SUCCESS'
 
-interface IActions {
-  TODOLIST_SUCCESS: string;
-}
+// interface IActions {
+//   TODOLIST_SUCCESS: string;
+// }
 
-interface todoListType {
-  type: IActions['TODOLIST_SUCCESS']
-  todoList?: todoItem[]
-}
+// interface todoListType {
+//   type: IActions['TODOLIST_SUCCESS']
+//   todoList?: todoItem[]
+// }
 
-export function fetchTodoListPending () {
-  console.log('fetch pending')
-  return {
-    type: TODOLIST_PENDING
-  }
-}
+// export function fetchTodoListPending () {
+//   console.log('fetch pending')
+//   return {
+//     type: TODOLIST_PENDING
+//   }
+// }
 
-export function fetchTodoListSuccess (todos: todoItem[]) {
-  return {
-    type: TODOLIST_SUCCESS,
-    todos
-  }
-}
+// export function fetchTodoListSuccess (todos: todoItem[]) {
+//   return {
+//     type: TODOLIST_SUCCESS,
+//     todos
+//   }
+// }
 
-export function fetchTodoList (token: string) {
-  console.log('fetchTodoList hit')
-  // what is dispatch type ???
-  return (dispatch: (args: todoListType)=> (todoListType)) => {
-    dispatch(fetchTodoListPending())
-    return getAllTodosAPI(token)
-      .then(todolist => {
-        console.log('fetch func action hit')
-        dispatch(fetchTodoListSuccess(todolist))
-        return null
-      })
-      .catch(err => {
-        console.error(err)
-        const errMessage = err.response?.text || err.message
-        dispatch(showError(errMessage))
-      })
-  }
-}
+// export function fetchTodoList (token: string) {
+//   console.log('fetchTodoList hit')
+//   // what is dispatch type ???
+//   return (dispatch: (args: todoListType)=> (todoListType)) => {
+//     dispatch(fetchTodoListPending())
+//     return getAllTodosAPI(token)
+//       .then(todolist => {
+//         console.log('fetch func action hit')
+//         dispatch(fetchTodoListSuccess(todolist))
+//         return null
+//       })
+//       .catch(err => {
+//         console.error(err)
+//         const errMessage = err.response?.text || err.message
+//         dispatch(showError(errMessage))
+//       })
+//   }
+// }
 
 // export function fetchTodos (token: string) {
 //   return (dispatch: DispatchTodoType) => {
