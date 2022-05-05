d1f3b0aed0e9ad50caacbc1e7ad561b28b756d72
bz888 <ziyu.ben158@gmail.com>
2022-05-05 13:10:48 +1200

    scripts

--- client/reducers/index.ts
+++ client/reducers/index.ts
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
 import user from './user'
-import { todoList } from './todos'
+// import { todoList } from './todos'
 
 export default combineReducers({
-  user: user,
-  todoList: todoList
+  user: user
+  // todoList: todoList
 })
